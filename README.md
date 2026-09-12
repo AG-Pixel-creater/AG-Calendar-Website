2f5a7d1e8fef25c876417b5713b00ebe08b33715
Muhammad Hamza Sabir <hamza.datashare@gmail.com>
2026-09-12 05:56:56 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AG-Calendar-Website
---
+++ config.js
@@ -0,0 +1,12 @@
+export const appConfig = {
+  name: "AG Calendar",
+  version: "1.0.0",
+  platform: "Windows",
+  github: {
+    repository: "AGCalendar/AG-Calendar",
+    releases: "https://github.com/AGCalendar/AG-Calendar/releases"
+  },
+  downloads: {
+    windows: "https://github.com/AGCalendar/AG-Calendar/releases/latest/download/AG-Calendar-Setup.exe"
+  }
+};
